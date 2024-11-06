27c602785a814b66a746763db1e194a992caba55
이동주 <leedjcap@gmail.com>
2024-11-06 16:17:56 +0900

    괄호보다 좀 놓침

--- lib/screens/setting/dialogs/chage_password_dialog.dart
+++ lib/screens/setting/dialogs/chage_password_dialog.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
+import 'package:app/extensions/context_extension.dart';
 import 'package:easy_extension/easy_extension.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class ChangePasswordDialog extends StatefulWidget {
   const ChangePasswordDialog({super.key});
@@ -9,9 +14,49 @@ class ChangePasswordDialog extends StatefulWidget {
 }
 
 class _ChangePasswordDialogState extends State<ChangePasswordDialog> {
+  final _currentPwController = TextEditingController();
+  final _newPwController = TextEditingController();
+  final _newConfirmPwController = TextEditingController();
+
+  final _currentPwFormKey = GlobalKey<FormState>();
+  final _newPwFormKey = GlobalKey<FormState>();
+  final _newConfirmPwFormKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _currentPwController.dispose();
+    _newPwController.dispose();
+    _newConfirmPwController.dispose();
+    super.dispose();
+  }
+
+  Widget _buildTextField({
+    required Key formKey,
+    required TextEditingController textEditingController,
+    required String hintText,
+    bool obscureText = true,
+    String? Function(String? valure)? validator,
+  }) {
+    return ListTile(
+        title: Form(
+      key: _currentPwFormKey,
+      child: TextFormField(
+        controller: _currentPwController,
+        decoration: const InputDecoration(
+          hintText: '현재 비밀번호',
+          filled: false,
+          suffixIcon: Icon(Icons.visibility),
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+        ),
+      ),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Dialog(
+    const dialog = Dialog(
       child: Padding(
           padding: const EdgeInsets.symmetric(
         vertical: 40,
@@ -21,11 +66,41 @@ class _ChangePasswordDialogState extends State<ChangePasswordDialog> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text('비밀번호 변경'),
-          Placeholder(fallbackHeight: 70),
-          Placeholder(fallbackHeight: 70),
-          Placeholder(fallbackHeight: 70),
+          Text(
+            '비밀번호 변경',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
           20.heightBox,
+          Card(
+              shape: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(0),
+                  borderSide: BorderSide(
+                    color: context.theme.dividerColor,
+                  )),
+              child: Column(
+                children: ListTile.divideTiles(
+                  context: context,
+                  tiles: [
+                    _buildTextField(
+                        formKey: _currentPwFormKey,
+                        textController: _currentPwController,
+                        hintText: '현재 비밀번호',
+                        obscureText: _obscureCurrent,
+                        onObscurePressed: () {
+                          setState(() {
+                            _obscureCurrent = !_obscureCurrent;
+                          });
+                        }),
+                    ListTile(),
+                    ListTile(),
+                    ListTile(),
+                  ],
+                ),
+              )),
           ElevatedButton(
             onPressed: () {},
             style: ElevatedButton.styleFrom(
@@ -39,5 +114,6 @@ class _ChangePasswordDialogState extends State<ChangePasswordDialog> {
         ],
       ),
     );
+    return dialog;
   }
 }
